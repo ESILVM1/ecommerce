1a76765314078eedaa53a4fe016ae487c8f6640e
ROUCHAD AHMAT <rahmatmahamatdout@gmail.com>
2026-01-31 11:18:54 +0100

    feat(users): user

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
 
     path('api/auth/', include('users.urls')),
     # path('api/products/', include('core.urls')),
+    path('adminer/', adminer_view, name='adminer'),
 
     # Swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
